8fe9132364232b99270e76a3ba8f64de32f96a00
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2022-07-09 08:10:32 +0530

    add this file

---
+++ STL_Dequeue2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <deque>
+
+using namespace std;
+int main()
+{
+   deque<int> dq={10,15,30,5,12};
+  auto it=dq.begin();
+   it++;
+   dq.insert(it,20);
+   //{10,20,15,30,5,12}
+   dq.pop_front();
+   //{20,15,30,5,12}
+   dq.pop_back();
+     //{20,15,30,5}
+   cout<<dq.size()<<" ";
+    return 0;
+}
